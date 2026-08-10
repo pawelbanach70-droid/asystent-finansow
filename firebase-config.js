414aac8ce2d2f1ca601c5573ab33818cdc7f7e29
Pawel <pawelbanach70@gmail.com>
2026-08-10 21:34:48 +0200

    Uzupelnij prawdziwa konfiguracje Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Wklej tutaj dane swojego projektu Firebase (Project settings -> Your apps -> Web app -> SDK setup and configuration).
 // Te wartości NIE są tajne - bezpieczeństwo zapewniają reguły Firestore, nie ukrywanie tego pliku.
 const firebaseConfig = {
-  apiKey: "TWOJ_API_KEY",
-  authDomain: "TWOJ_PROJEKT.firebaseapp.com",
-  projectId: "TWOJ_PROJEKT",
-  storageBucket: "TWOJ_PROJEKT.appspot.com",
-  messagingSenderId: "TWOJ_SENDER_ID",
-  appId: "TWOJ_APP_ID"
+  apiKey: "AIzaSyAuHkiFVBYsjWp5ZC2rhU4dM9Ym3Ckcwec",
+  authDomain: "asystent-finansow.firebaseapp.com",
+  projectId: "asystent-finansow",
+  storageBucket: "asystent-finansow.firebasestorage.app",
+  messagingSenderId: "773374468671",
+  appId: "1:773374468671:web:57725270c46878168d4e8a"
 };
 
 firebase.initializeApp(firebaseConfig);
